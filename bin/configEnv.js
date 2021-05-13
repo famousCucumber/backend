ee0527a84ee67f8b7703c8c48abd8c8f7a9034de
Eunhak Lee <lee@enak.kr>
2021-05-13 19:08:22 +0900

    chore: 이메일 데몬 config 추가

--- bin/configEnv.js
+++ bin/configEnv.js
@@ -2,6 +2,10 @@ const configEnv = function() {
     process.env.DB_USERNAME = '';
     process.env.DB_PW = '';
     process.env.DB_PATH = '';
+    process.env.EMAIL_HOST = 'smtp.gmail.com';
+    process.env.EMAIL_PORT = 587;
+    process.env.EMAIL_USERNAME = '';
+    process.env.EMAIL_PASSWORD = '';
 }
 
 module.exports = configEnv;
